a2f4c8133a73e6c970ae25d45116129c91271cd1
Falon <scheers@ualberta.ca>
2016-03-02 16:46:45 -0700

    jacobian stuff done

--- src/RobotController.java
+++ src/RobotController.java
@@ -1,19 +1,43 @@
 import lejos.utility.Matrix;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
 public class RobotController {
+    private static RegulatedMotor m_motorA;
+	private static RegulatedMotor m_motorB;
+
+  	private static final int motor_speed = 70;
+	private static final int motor_accel = 30;
+	
+	private static final double[] gear_ratios = {15d/30d, 1d, -1d};
+
 	/**
 	 * Moves the joints slightly to find an estimate for
 	 * the jacobian at the current location.
 	 * @return
 	 */
-	Matrix estimateJacobian() {
-		// for each joint
-		//	   y0 = getTrackerPosition()
-		//     move joint slightly
-		//	   y1 = getTrackerPosition()
-		//	   insert y1 - y0 into the jacobian as a column vector
-		// return J
-		return null;
+	public static Matrix estimateJacobian() {
+        Matrix J = new Matrix(2,2);
+        double deltaTheta = 0.0 // wiggle this many degrees to get initial J
+        RegulatedMotor motorA=GetMotorA();
+        RegulatedMotor motorB=GetMotorB();
+        RegulatedMotor motors= new RegulatedMotor[] {motorA,MotorB};
+        
+        for (int j=0; j<2; j++){
+            double q0 = VisualMain.getTrackerPosition();
+            motors[j].rotateTo(deltaTheta);
+            // may need a delay in here??
+            Delay.msdelay(100)
+            double q1 = VisualMain.getTrackerPosition();
+            J.set(0, j, q1.x-q0.x/deltaTheta);
+            J.set(1, j, q1.y-q1.y/deltaTheta);
+            //move joints back after
+            motors[j].rotateTo(-deltaTheta);
+        }
+     	return J;
 	}
 	
 	/**
@@ -30,4 +54,20 @@ public class RobotController {
 		//    J = VisualKinematics.broydenUpdate(J, dx, error) // CHECK which dx and error this is
 	}
 	
+    /**
+	 * Gets motorA as a singleton. Prevents the port from being opened twice.
+	 */
+	private static RegulatedMotor getMotorA() {
+		if (m_motorA == null)
+			m_motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+		return m_motorA;
+	}
+	/**
+	 * Gets motorB as a singleton. Prevents the port from being opened twice.
+	 */
+	private static RegulatedMotor getMotorB() {
+		if (m_motorB == null)
+			m_motorB = new EV3LargeRegulatedMotor(MotorPort.B);
+		return m_motorB;
+	}
 }
